c1744999e8ab548769c36a4b9e270295cce0846f
snehasingh-2 <139580847+snehasingh-2@users.noreply.github.com>
2023-10-31 21:35:50 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{  
     public static void main(String[] args){  
         System.out.println("This is java app \n by using git hub"); 
+        System.out.println("Changes Done with git"); 
     }  
 }  
