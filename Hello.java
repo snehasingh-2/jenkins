1a179a9578211ce4e3ebe1dbb78b2555add5f3e2
snehasingh-2 <139580847+snehasingh-2@users.noreply.github.com>
2023-10-31 12:19:18 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{  
     public static void main(String[] args){  
     System.out.println("This is java app \n by using Docker");  
+        System.out.println("This is java app \n by using git hub"); 
     }  
 }  
